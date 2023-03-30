ee6c0c027065b5ccd4718a204911ea579c31f75c
Ariflutfi <arif.kurniawan@students.amikom.ac.id>
2023-03-30 16:18:50 +0700

    commit

--- src/main/kotlin/example/myapp/Main.kt
+++ src/main/kotlin/example/myapp/Main.kt
@@ -2,8 +2,22 @@ package example.myapp
 
 fun buildAquarium() {
     /** instance, mewarisi properti yang dimiliki oleh Aquarium() **/
-    val myAquarium = Aquarium()
-    myAquarium.printSize()
+//    val myAquarium = Aquarium(numberOfFish = 29)
+//    myAquarium.printSize()
+//    // default height and length
+//    val aquarium2 = Aquarium(width = 25)
+//    aquarium2.printSize()
+//    // default width
+//    val aquarium3 = Aquarium(height = 35, length = 110)
+//    aquarium3.printSize()
+//    // everything custom
+//    val aquarium4 = Aquarium(width = 25, height = 35, length = 110)
+//    aquarium4.printSize()
+
+    val aquarium6 = Aquarium(numberOfFish = 29)
+    aquarium6.printSize()
+    aquarium6.volume = 70
+    aquarium6.printSize()
 }
 
 fun main() {
